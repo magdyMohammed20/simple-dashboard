d8b8218cf687b3d1f1d7757072d50cbcbb993469
magdyMohammed20 <magdymohammed37@yahoo.com>
2023-11-07 12:43:18 +0200

    clean up

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -5,7 +5,8 @@ import { faker } from '@faker-js/faker';
 
 export const users = [...Array(24)].map((_, index) => ({
   id: faker.string.uuid(),
-  avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
+  ID: faker.string.uuid(),
+  // avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
   name: faker.person.fullName(),
   company: faker.company.name(),
   isVerified: faker.datatype.boolean(),
@@ -18,7 +19,7 @@ export const users = [...Array(24)].map((_, index) => ({
     'UI/UX Designer',
     'Project Manager',
     'Backend Developer',
-    'Full Stack Designer',
+    'Full Stack Designer111',
     'Front End Developer',
     'Full Stack Developer',
   ]),
